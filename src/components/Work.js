2237a715224a19699fc1506bd9c1f5c86adc809b
vasilisa <igorevna.vasilisa@gmail.com>
2019-08-09 09:47:50 +0200

    play button

--- src/components/Work.js
+++ src/components/Work.js
@@ -20,7 +20,19 @@ export default class extends Component {
                     <p dangerouslySetInnerHTML={{ __html: block.description }}></p>
                   </div>
                   <div className='work--video-watch'>
-                    <a href={block.videoLink} target='_blank' rel='noopener noreferrer' className='work--video-watch-link'><span className='work--video-watch-icon'>&#9654;</span>watch trailer</a>
+                    <a href={block.videoLink} target='_blank' rel='noopener noreferrer' className='work--video-watch-link'>
+                      <span className='work--video-watch-icon'>
+                        <svg width="100%" height="100%" viewBox="0 0 50 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" xmlSpace="preserve" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2">
+                          <rect id="play" x="0" y="0" width="50" height="62" fill="none"/>
+                          <clipPath id="play-button">
+                            <rect x="0" y="0" width="50" height="62"/>
+                          </clipPath>
+                          <g clipPath="url(#play-button)">
+                            <path d="M0,0l50,31l-50,31l0,-62Z" fillRule="nonzero"/>
+                          </g>
+                        </svg>
+                      </span>
+                      watch trailer</a>
                   </div>
                 </div>
               )
